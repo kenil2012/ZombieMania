ae11e3a26103c5daa34db3415d17ba0a97fb0597
Kenil <kenil201446@gmail.com>
2020-05-04 04:06:45 +0530

    Updates

---
+++ precache-manifest.703bdbe67ef0ac34b4f9ac3e241521c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57105c0f008a1deb13b4015aaa0046e7",
+    "url": "/ZombieMania/index.html"
+  },
+  {
+    "revision": "2d218f22554ab2cae9af",
+    "url": "/ZombieMania/static/css/main.6277a895.chunk.css"
+  },
+  {
+    "revision": "422d061d726a49841a1c",
+    "url": "/ZombieMania/static/js/2.2111b635.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ZombieMania/static/js/2.2111b635.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d218f22554ab2cae9af",
+    "url": "/ZombieMania/static/js/main.30f84861.chunk.js"
+  },
+  {
+    "revision": "3a341194e1216d012b88",
+    "url": "/ZombieMania/static/js/runtime-main.1388f82c.js"
+  }
+]);
